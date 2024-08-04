79e079c1063b6683ca934afcbdb25919318649c6
Justin Bench <67075151+jdbench@users.noreply.github.com>
2024-08-04 07:18:30 -0600

    Add code prep and test job (#4)
    * Add code prep and test job
    
    * add ci
    
    * rename yaml and do a clean install step
    
    * try deploy step
    
    * add environments
    
    * see if this works
    
    * use quotation marks
    
    * try build arg
    
    * generate toml
    
    * remove fly toml from git
    
    * Delete fly.toml
    
    * try this instead
    
    * get rid of hyphen
    
    * try this
    
    * try this
    
    * two exports
    
    * no exports
    
    * see if push is in scope
    
    * give this a shot
    
    * get from needs
    
    * use latest
    
    * append pr number
    
    * latest versioning on scripts

---
+++ fly.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "app = '$VAR_FLY_APP_NAME'
+primary_region = 'sea'
+
+[build]
+  image = '$VAR_FLY_IMAGE'
+
+[http_service]
+  internal_port = 3000
+  force_https = true
+  auto_stop_machines = 'stop'
+  auto_start_machines = true
+  min_machines_running = 0
+  processes = ['app']
+
+[[vm]]
+  memory = '1gb'
+  cpu_kind = 'shared'
+  cpus = 1
+" > fly.toml
+
+echo $vars
+echo $env
