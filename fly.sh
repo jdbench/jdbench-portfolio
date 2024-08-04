069297b01374b7e633f5286783d81115b54b3055
Justin Bench <justin.d.bench@gmail.com>
2024-08-04 06:10:45 -0600

    try this instead

--- fly.sh
+++ fly.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-echo "
-# fly.toml app configuration file generated for jdbench-portfolio on 2024-08-04T04:41:05-06:00
-#
-# See https://fly.io/docs/reference/configuration/ for information about how to use this file.
-#
-
-app = '$VAR_FLY_APP_NAME'
+echo "app = '$VAR_FLY_APP_NAME'
 primary_region = 'sea'
 
 [build]
