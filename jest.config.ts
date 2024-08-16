cf5b21497cffee6ff5d624087b34844f467019b4
Justin Bench <jdbench@churchofjesuschrist.org>
2024-08-16 05:09:50 -0600

    more config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,9 @@
 import type { Config } from "jest";
 const config: Config = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    "**/*.{js,jsx}",
-    "!**/node_modules/**",
-    "!**/next/**",
-  ],
+  collectCoverageFrom: ["**/*.{js,jsx}", "!**/node_modules/**", "!**/next/**"],
   coverageDirectory: "coverage",
+  coverageReporters: ["clover", "json", "lcov", ["text", { skipFull: true }]],
   clearMocks: true,
   verbose: true,
   testEnvironment: "jsdom",
