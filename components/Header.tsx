d36255537b5b96579bb91310a1808cfa839253f7
Justin Bench <jdbench@churchofjesuschrist.org>
2024-11-28 21:12:13 -0700

    add some styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
-export default function Header({ showNav }: { showNav: boolean }) {
-  return (
-    <header className={`header${showNav ? " show" : ""}`}>
-      <h1><a href="#dashboard">Justin Bench</a></h1>
-    </header>
-  );
+export default function Header({
+    showNav,
+    toggleNav,
+}: {
+    showNav: boolean;
+    toggleNav: () => {};
+}) {
+    return (
+        <header className={`header${showNav ? " show" : ""}`}>
+            <h1 onClick={toggleNav}>
+                <a href="#dashboard">Justin Bench</a>
+            </h1>
+        </header>
+    );
 }
--- components/LeftColumn.tsx
+++ components/LeftColumn.tsx
@@ -7,43 +7,33 @@ import { FaBars } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
 export default function LeftColumn({
-  navList,
+    navList,
 }: {
-  navList: { href: string; title: string }[];
+    navList: { href: string; title: string }[];
 }) {
-  const [showNav, setShowNav] = useState(false);
+    const [showNav, setShowNav] = useState(false);
 
-  const toggleNav = () => {
-    setShowNav(!showNav);
-  };
+    const toggleNav = () => {
+        setShowNav(!showNav);
+    };
 
-  return (
-    <>
-      <div className="icons">
-        {showNav ? (
-          <IoMdClose
-            color="white"
-            onClick={toggleNav}
-            size={24}
-          />
-        ) : (
-          <FaBars
-            onClick={toggleNav}
-            size={24}
-          />
-        )}
-      </div>
-      <div
-        id="left-column"
-        className={`body-left${showNav ? " flex-show" : ""}`}>
-        <Header showNav={showNav} />
-        <Nav
-          items={navList}
-          showNav={showNav}
-          toggleNav={toggleNav}
-        />
-        <Footer showNav={showNav} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="icons">
+                {showNav ? (
+                    <IoMdClose color="white" onClick={toggleNav} size={24} />
+                ) : (
+                    <FaBars onClick={toggleNav} size={24} />
+                )}
+            </div>
+            <div
+                id="left-column"
+                className={`body-left${showNav ? " flex-show" : ""}`}
+            >
+                <Header showNav={showNav} toggleNav={toggleNav} />
+                <Nav items={navList} showNav={showNav} toggleNav={toggleNav} />
+                <Footer showNav={showNav} />
+            </div>
+        </>
+    );
 }
